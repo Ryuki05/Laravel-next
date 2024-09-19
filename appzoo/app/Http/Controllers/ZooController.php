302d7f42e0f13bab2c1d0d2750dba7c12e65e726
Ryuki05 <higashiking05@gmail.com>
2024-09-19 19:37:25 +0900

    Controller and api.php add

--- appzoo/app/Http/Controllers/ZooController.php
+++ appzoo/app/Http/Controllers/ZooController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 use App\Models\Zoo;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreZooRequest;
@@ -14,7 +14,12 @@ class ZooController extends Controller
      */
     public function index()
     {
-        //
+        //inndexは一覧取得するメソッド
+        //Zooはモデル名
+        $animals = Zoo::all();
+
+        //json形式で$animalsをレスポンス
+        return response()->json($animals);
     }
 
     /**
@@ -22,7 +27,8 @@ class ZooController extends Controller
      */
     public function create()
     {
-        //
+        //Apiの場合特に何も返さないことが多い
+        return response()->json(['message' => 'Ready to create a new zoo']);
     }
 
     /**
@@ -30,38 +36,64 @@ class ZooController extends Controller
      */
     public function store(StoreZooRequest $request)
     {
-        //
+        //バリデーション
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'location' => 'required|string|max:255'
+        ]);
+
+        //新しいZooを作成して保存
+        $zoo = Zoo::create($validatedData);
+
+        //成功した場合のレスポンス
+        return response()->json($zoo,201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Zoo $zoo)
+    public function show($id)
     {
-        //
+        $zoo = Zoo::findOrFail($id);
+
+        return response()->json($zoo);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Zoo $zoo)
+    public function edit($id)
     {
-        //
+        $zoo = Zoo::findOrFail($id); //リソースが見つからない場合は404エラーを返す
+
+        return response()->json($zoo);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateZooRequest $request, Zoo $zoo)
+    public function update(Request $request, $id)
     {
-        //
+        //バリデーション
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'location' => 'required|string|max:255'
+        ]);
+
+        $zoo = Zoo::findOrFail($id); //リソースが見つからない場合は404エラーを返す
+        $zoo->update($validatedData);
+
+        return response()->json($zoo);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Zoo $zoo)
+    public function destroy($id)
     {
-        //
+        $zoo = Zoo::findOrFail($id); //リソースが見つからない場合は404エラーを返す
+        $zoo->delete(); //データを削除
+
+        return response()->json($zoo); //202 No Content
     }
 }
